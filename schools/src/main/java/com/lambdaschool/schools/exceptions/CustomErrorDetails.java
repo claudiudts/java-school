890c6ed805f8c730d26f37b0d55d7e472d2faae6
Claudiuts <sarb.claudiu391@gmail.com>
2021-04-20 10:17:33 -0500

    error details

--- schools/src/main/java/com/lambdaschool/schools/exceptions/CustomErrorDetails.java
+++ schools/src/main/java/com/lambdaschool/schools/exceptions/CustomErrorDetails.java
@@ -5,6 +5,8 @@ import org.springframework.boot.web.servlet.error.DefaultErrorAttributes;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.request.WebRequest;
 
+import java.util.Date;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 @Component
@@ -13,10 +15,21 @@ public class CustomErrorDetails extends DefaultErrorAttributes
     @Override
     public Map<String, Object> getErrorAttributes(WebRequest webRequest, boolean includeStackTrace)
     {
+
+        Map<String, Object> errorAttributes = super.getErrorAttributes(webRequest, includeStackTrace);
         //title: JSON OBJ
         //status: the HTTP
         //detail: message what the error is
         //timestamp: when it happened
         //developerMessage: some routing info, for developers
+        Map<String, Object> rtnAttributes = new LinkedHashMap<>();
+
+        rtnAttributes.put("title", errorAttributes.get("error"));
+        rtnAttributes.put("status", errorAttributes.get("status"));
+        rtnAttributes.put("detail", errorAttributes.get("message"));
+        rtnAttributes.put("timestamp", new Date());
+        rtnAttributes.put("developer", "path" + errorAttributes.get("path"));
+
+        return rtnAttributes;
     }
 }
